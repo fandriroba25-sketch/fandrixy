d6bc80a121183e6dcce28f037b90b6c4fc344f82
fandriroba25-sketch <fandriroba25@gmail.com>
2026-04-19 18:26:26 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function lanjut() {
+  alert("Terima kasih! Mengarahkan ke download...");
+  
+  // Ganti link download di sini
+  window.location.href = "https://link-download-kamu.com/file.zip";
+}
